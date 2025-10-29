0762eee6a9e0d3c3b1497198c620596284e5ba49
Masil Sanjai <sanjaipaxley@gmail.com>
2025-10-29 15:09:16 +0530

    Update StringCombination.java

--- StringCombination.java
+++ StringCombination.java
@@ -1,10 +1,11 @@
 public class StringCombination{
-	public static void StringArgument(String a, String b){
-		String c= " Technologies ";
+	public static void StringArgument(String a, String c){
+		String b= " Technologies ";
 		String result=a+b+c;
 		System.out.println(result);
 		}
 	public static void main (String args[]){
 		StringArgument("Wipro ","Banglore");
 		}
+
 	}
