1618d99bec3662f5d537fb78e1a768a375981a1c
Masil Sanjai <sanjaipaxley@gmail.com>
2025-10-29 15:05:52 +0530

    Homework
    Write a Program that accepts two Strings as command line arguments and generate the output in the required format.

---
+++ StringCombination.java
@@ -0,0 +1,10 @@
+public class StringCombination{
+	public static void StringArgument(String a, String b){
+		String c= " Technologies ";
+		String result=a+b+c;
+		System.out.println(result);
+		}
+	public static void main (String args[]){
+		StringArgument("Wipro ","Banglore");
+		}
+	}
