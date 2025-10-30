e7767c3500f86bef827a0d92de5565b502db36c8
Masil Sanjai <sanjaipaxley@gmail.com>
2025-10-30 14:53:07 +0530

    Create Sample.java

---
+++ Day-1/Sample.java
@@ -0,0 +1,11 @@
+// Program to print a Welcome message using command-line input
+public class Sample {
+    public static void main(String[] args) {
+        // Check if a name was provided as argument
+        if (args.length > 0) {
+            System.out.println("Welcome " + args[0]);
+        } else {
+            System.out.println("Please provide a name as a command-line argument.");
+        }
+    }
+}
