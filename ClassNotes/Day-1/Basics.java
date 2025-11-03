f22e465e8fa9efb208f973f4462a168b52bd4ad4
Masil Sanjai <sanjaipaxley@gmail.com>
2025-11-03 12:50:21 +0530

    Create Basics.java

---
+++ ClassNotes/Day-1/Basics.java
@@ -0,0 +1,7 @@
+public class Basics {
+  public static void main(String args[]){
+    System.out.println("This is printed by print statement");
+    System.out.println("Datatypes");
+    int num=20;
+    System.out.println("This is Integer %d",num);
+  }}
