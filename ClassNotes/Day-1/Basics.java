268a1e8ae00e95af83c5a2dbab4c89973e843f0e
Masil Sanjai <sanjaipaxley@gmail.com>
2025-11-03 14:11:44 +0530

    Update Basics.java

--- ClassNotes/Day-1/Basics.java
+++ ClassNotes/Day-1/Basics.java
@@ -1,4 +1,4 @@
-public class Basics {
+ public class Basics {
   public static void main(String args[]){
     System.out.println("This is printed by print statement");
     System.out.println("Datatypes");
@@ -10,4 +10,6 @@ public class Basics {
     System.out.printf("This is Long Integer %d\n",numlong);
     float numdecimal=3.56f;
     System.out.printf("this is Float %f\n",numdecimal);
-  }}
+    double numdouble=44.32;
+    System.out.printf("This is Double %f\n",numdouble);
+    }}
